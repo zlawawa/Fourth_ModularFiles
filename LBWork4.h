aab947e53253f77e5858dee2cbdc8888176ffa3b
zlawawa <zlatakyhalova@gmail.com>
2025-11-26 21:10:57 +0500

    Update LBWork4.h

--- LBWork4.h
+++ LBWork4.h
@@ -7,9 +7,16 @@
 void FillKeyboard(std::vector<std::vector<int>> &matrix);
 void FillRandom(std::vector<std::vector<int>> &matrix, int n, int m);
 void FillOutTFiles(std::vector<std::vector<int>> &matrix, int &n, int &m);
-// void ClearMatrix(std::vector<std::vector<int>> &matrix);
 
-int taskMatrix28(std::vector<std::vector<int>> &matrix);
+int taskMatrix28Console(std::vector<std::vector<int>> &matrix);
+void taskMatrix28File(std::vector<std::vector<int>> &matrix);
+
+int taskMatrix55(std::vector<std::vector<int>> &matrix);
+void taskMatrix55File(std::vector<std::vector<int>> &matrix);
+
+int taskDArray6(std::vector<std::vector<int>> &matrixA);
+int taskDArray6File(std::vector<std::vector<int>> &matrixA);
+
 int taskMatrix55();
 int taskDArray6();
 int taskDArray16();
